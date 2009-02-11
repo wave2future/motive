f29b0db88a33a829320a56c94e64f234afc957b1
Nick Partridge <nkpart@gmail.com>
2009-02-12 08:35:43 +1000

    Logging of integer/floats

--- Source/MVLog.h
+++ Source/MVLog.h
@@ -5,11 +5,15 @@
 #define TRACE() NSLog(CALL_POINT())
 #define LOG(args...) NSLog(@"%@", [NSString stringWithFormat:args])
 #define LOGV(varname) NSLog(@"%s = %@", #varname, varname)
+#define LOGD(varname) NSLog(@"%s = %d", #varname, varname)
+#define LOGF(varname) NSLog(@"%s = %d", #varname, varname)
 
 #else
 
 #define TRACE()
 #define LOG(args...)
 #define LOGV(varname)
+#define LOGD(varname)
+#define LOGF(varname)
 
 #endif
