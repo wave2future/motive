c9214100afb9e6ec1efc2c39f1e7a8c9a91a53ac
Tom Adams <tom@avari.local>
2009-02-20 13:40:18 +1000

    adding more tests

--- Source/Test/Network/MVHttpUnitTest.m
+++ Source/Test/Network/MVHttpUnitTest.m
@@ -8,10 +8,7 @@
 @implementation MVHttpUnitTest
 
 - (void)testEncodesAnEmptyDictionAryAsAnEmpty {
-//    NSMutableDictionary *params = [NSMutableDictionary dictionary];
-//    [params setValue:[NSNumber numberWithInt:1] forKey:@"foo"];
-//    [params setValue:@"somebar" forKey:@"bar"];
-//    STAssertEqualObjects(@"foo=1&bar=somebar", [MVHttp queryStringFromDictionary:params], nil);
+    STAssertEqualObjects(@"", [MVHttp queryStringFromDictionary:[NSDictionary dictionary]], nil);
 }
 
 - (void)testEncodesADictionaryAsAQueryString {
