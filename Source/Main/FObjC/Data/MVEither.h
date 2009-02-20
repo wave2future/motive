c95edd3a19a5b5f91f9072e8040272ba38892cb1
Tom Adams <tom@avari.local>
2009-02-20 15:22:57 +1000

    Added description & isEqual to either

--- Source/Main/FObjC/Data/MVEither.h
+++ Source/Main/FObjC/Data/MVEither.h
@@ -2,20 +2,21 @@
 
 // TODO Look at FJ projections for a better way to return an error, so we don't need to do: [(NSError *)[result value]  localizedDescription].
 // TODO Add in some projections, look at FJ for details.
+// TODO Add isEqual: method.
 
 // The Either type represents a value of one of two possible types (a disjoint union).
 // The data constructors; Left and Right represent the two possible values. The Either type is often used as an alternative to Option where Left 
 // represents failure (by convention) and Right is akin to Some.
 @interface MVEither : NSObject {
-    id object;
+    id value;
     BOOL onleft;
 }
 
 // Construct a left value of either.
-+ (MVEither *)leftWithObject:(id)object;
++ (MVEither *)leftWithValue:(id)value;
 
 // Construct a right value of either.
-+ (MVEither *)rightWithObject:(id)object;
++ (MVEither *)rightWithValue:(id)value;
 
 // Does this either have a value on the left?
 - (BOOL)isLeft;
--- Source/Main/FObjC/Data/MVEither.m
+++ Source/Main/FObjC/Data/MVEither.m
@@ -1,17 +1,17 @@
 #import "MVEither.h"
 
 @interface MVEither (MVEitherPrivate)
-- (MVEither *)initWithObject:(id)object onLeft:(BOOL)onLeft;
+- (MVEither *)initWithValue:(id)value onLeft:(BOOL)onLeft;
 @end
 
 @implementation MVEither
 
-+ (MVEither *)leftWithObject:(id)object {
-    return [[[MVEither alloc] initWithObject:object onLeft:YES] autorelease];
++ (MVEither *)leftWithValue:(id)value {
+    return [[[MVEither alloc] initWithValue:value onLeft:YES] autorelease];
 }
 
-+ (MVEither *)rightWithObject:(id)object {
-    return [[[MVEither alloc] initWithObject:object onLeft:NO] autorelease];
++ (MVEither *)rightWithValue:(id)value {
+    return [[[MVEither alloc] initWithValue:value onLeft:NO] autorelease];
 }
 
 - (BOOL)isLeft {
@@ -23,18 +23,36 @@
 }
 
 - (id)value {
-    return object;
+    return value;
+}
+
+#pragma mark NSObject methods.
+- (BOOL)isEqual:(id)object {
+    if (object == nil || ![[object class] isEqual:[MVEither class]]) {
+        return NO;
+    } else {
+        MVEither *other = (MVEither *) object;
+        return [self isLeft] == [other isLeft] && [[self value] isEqual:[other value]];
+    }
+}
+
+- (NSUInteger)hash {
+    return value == nil ? 42 : [value hash];
+}
+
+- (NSString *)description {
+    return [NSString stringWithFormat:@"<MVEither isLeft: %@; value: %@>", onleft ? "true" : "false", value];
 }
 
 - (void)dealloc {
-    [object release];
+    [value release];
     [super dealloc];
 }
 
 #pragma mark Private methods.
-- (MVEither *)initWithObject:(id)newObject onLeft:(BOOL)newOnLeft {
+- (MVEither *)initWithValue:(id)newValue onLeft:(BOOL)newOnLeft {
     if (self = [super init]) {
-        object = [newObject retain];
+        value = [newValue retain];
         onleft = newOnLeft;
     }
     return self;
