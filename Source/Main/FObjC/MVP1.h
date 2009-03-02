263d6a89a780c6da8e3ac8c7fecd7b898f5fd906
Tom Adams <tom@avari.local>
2009-03-03 08:48:36 +1000

    Added p1

---
+++ Source/Main/FObjC/MVP1.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+@interface MVP1 : NSObject {
+    id _1;
+}
+
+READ id _1;
+
++ (MVP1 *)p1With_1:(id)_1;
+
+@end
---
+++ Source/Main/FObjC/MVP1.m
@@ -0,0 +1,28 @@
+#import "MVP1.h"
+
+@interface MVP1 (MVP1Private)
+- (MVP1 *)initWith_1:(id)new_1;
+@end
+
+@implementation MVP1
+
+@synthesize _1;
+
++ (MVP1 *)p1With_1:(id)_1 {
+    return [[[MVP1 alloc] initWith_1:_1] autorelease];
+}
+
+- (void) dealloc {
+    [_1 release];
+    [super dealloc];
+}
+
+#pragma mark Private methods.
+- (MVP1 *)initWith_1:(id)new_1 {
+    if (self = [super init]) {
+        _1 = [new_1 retain];
+    }
+    return self;
+}
+
+@end
