a536e0b8f6b6ec2c20e35a15d5e7600cec22fa0a
Tom Adams <tomjadams@gmail.com>
2009-05-29 14:43:20 +1000

    fixed up view centring

---
+++ Source/Main/MV/UIView+Motive.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+@interface UIView (MotiveExtensions)
+
+// Moves this view to the centre (vertically & horizontally) of |view|.
+- (void)centreWithinView:(UIView *)view;
+
+// Moves this view to the top (vertically) and centre (horizontally) of |view|.
+- (void)moveToTopCentreOfView:(UIView *)view;
+
+@end
